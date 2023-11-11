6f7330f93bec65afe3b178baa96349d36b245d72
Ivan Osipchyk <ivanosipchyk@MacBook-Air.local>
2023-11-11 13:36:15 +0100

    added existing code to fillers

--- c04_osipchyk_ivan/src/fill/ScanLine.java
+++ c04_osipchyk_ivan/src/fill/ScanLine.java
@@ -1,10 +1,75 @@
 package fill;
 
-public class ScanLine implements Filler {
+import model.Edge;
+import model.Line;
+import model.Point;
+import model.Polygon;
+import rasterize.LineRasterizer;
+import rasterize.PolygonRasterizer;
+import rasterize.Raster;
 
+import java.util.ArrayList;
+import java.util.Collections;
 
+public class ScanLine implements Filler{
+    private LineRasterizer lineRasterizer;
+    private Polygon polygon;
+    private PolygonRasterizer polygonRasterizer;
+
+    public ScanLine(LineRasterizer lineRasterizer, PolygonRasterizer polygonRasterizer, Polygon polygon) {
+        this.polygon = polygon;
+        this.lineRasterizer = lineRasterizer;
+        this.polygonRasterizer = polygonRasterizer;
+    }
     @Override
     public void fill() {
+        ArrayList<Edge> edges = new ArrayList<>();
+        for (int i = 0; i < polygon.size(); i++) {
+            Point p1 = polygon.getPoint(i);
 
+            int indexB = i + 1;
+            if (indexB == polygon.size())
+                indexB = 0;
+
+            Point p2 = polygon.getPoint(indexB);
+
+            Edge edge = new Edge(p1.x, p1.y, p2.x, p2.y);
+            if (edge.isHorizontal())
+                continue;
+
+            edges.add(edge);
+        }
+
+        int yMin = polygon.getPoint(0).y;
+        int yMax = yMin;
+
+        for (Point point: polygon.getPoints()) {
+            int currY = point.y;
+
+            yMin = Math.min(currY, yMin);
+            yMax = Math.max(currY, yMax);
+        }
+
+        for (int y = yMin; y <= yMax; y++) {
+            ArrayList<Integer> intersections = new ArrayList<>();
+
+            for (Edge edge: edges) {
+                if (edge.getY1() < y && edge.getY2() >= y || edge.getY2() < y && edge.getY1() >= y) {
+                    int x = (int) Math.round(edge.getX1() + (double) (y - edge.getY1()) / (edge.getY2() - edge.getY1()) * (edge.getX2() - edge.getX1()));
+                    intersections.add(x);
+                }
+            }
+
+            Collections.sort(intersections);
+
+            for (int i = 0; i < intersections.size(); i+=2) {
+                int x1 = intersections.get(i);
+                int x2 = intersections.get(i+1);
+
+                lineRasterizer.rasterize(x1, y, x2, y);
+            }
+        }
+
+        polygonRasterizer.rasterize(polygon);
     }
 }
--- c04_osipchyk_ivan/src/fill/SeedFill.java
+++ c04_osipchyk_ivan/src/fill/SeedFill.java
@@ -1,9 +1,34 @@
 package fill;
 
-public class SeedFill implements Filler {
+import rasterize.Raster;
 
+public class SeedFill implements Filler{
+    private Raster raster;
+    private int x, y;
+    private int backgroundColor;
+
+    public SeedFill(Raster raster, int backgroundColor, int x, int y) {
+        this.raster = raster;
+        this.backgroundColor = backgroundColor;
+        this.x = x;
+        this.y = y;
+    }
     @Override
     public void fill() {
+        seedFill(x, y);
+    }
 
+    private void seedFill(int x, int y) {
+        int pixelColor = raster.getPixel(x, y);
+
+        if (this.backgroundColor != pixelColor)
+            return;
+
+        raster.setPixel(x, y, 0xff00ff);
+
+        seedFill(x+1, y);
+        seedFill(x-1, y);
+        seedFill(x, y+1);
+        seedFill(x, y-1);
     }
 }
