dcab586e488a00ab48f01200de3d2ffe97445ad7
Ivan Osipchyk <ivanosipchyk@MacBook-Air.local>
2023-11-11 13:36:40 +0100

    added existing code to models

--- c04_osipchyk_ivan/src/model/Edge.java
+++ c04_osipchyk_ivan/src/model/Edge.java
@@ -1,2 +1,44 @@
-package fill;public class Edge {
+package model;
+
+public class Edge {
+    private int x1, x2, y1, y2;
+
+    public Edge(int x1, int y1, int x2, int y2) {
+        this.x1 = x1;
+        this.x2 = x2;
+        this.y1 = y1;
+        this.y2 = y2;
+    }
+
+    public boolean isHorizontal() {
+        return y1 == y2;
+    }
+
+    public void orientate() {
+        if (y2 < y1) {
+            int temp = x1;
+            x1 = x2;
+            x2= temp;
+
+            temp = y1;
+            y1 = y2;
+            y2= temp;
+        }
+    }
+
+    public int getX1() {
+        return this.x1;
+    }
+
+    public int getY1() {
+        return this.y1;
+    }
+
+    public int getX2() {
+        return this.x2;
+    }
+
+    public int getY2() {
+        return this.y2;
+    }
 }
--- c04_osipchyk_ivan/src/model/Rectangle.java
+++ c04_osipchyk_ivan/src/model/Rectangle.java
@@ -1,2 +1,17 @@
-package model;public class Rectangle {
+package model;
+
+import java.util.ArrayList;
+
+public class Rectangle extends Polygon {
+    private ArrayList<Point> points;
+
+    public Rectangle(Point p1, Point p3) {
+        Point p2 = new Point(p1.x, p3.y);
+        Point p4 = new Point(p3.x, p1.y);
+
+        addPoint(p1);
+        addPoint(p2);
+        addPoint(p3);
+        addPoint(p4);
+    }
 }
