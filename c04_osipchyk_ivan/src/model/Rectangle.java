541a433d31013b5cd0240277bd456b25e3d05a48
Ivan Osipchyk <ivanosipchyk@MacBook-Air.local>
2023-11-11 22:10:18 +0100

    added new constructor and method to add points

--- c04_osipchyk_ivan/src/model/Rectangle.java
+++ c04_osipchyk_ivan/src/model/Rectangle.java
@@ -14,4 +14,18 @@ public class Rectangle extends Polygon {
         addPoint(p3);
         addPoint(p4);
     }
+
+    public Rectangle() {
+
+    }
+
+    public void addAllPoints(Point p1, Point p3) {
+        Point p2 = new Point(p1.x, p3.y);
+        Point p4 = new Point(p3.x, p1.y);
+
+        addPoint(p1);
+        addPoint(p2);
+        addPoint(p3);
+        addPoint(p4);
+    }
 }
