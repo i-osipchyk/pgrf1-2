a4b84b2f66825bb6f92ecdc8f8f6d82482be3e14
Ivan Osipchyk <ivanosipchyk@MacBook-Air.local>
2023-11-13 22:41:23 +0100

    handled out of bounds exception

--- c04_osipchyk_ivan/src/fill/SeedFill.java
+++ c04_osipchyk_ivan/src/fill/SeedFill.java
@@ -4,14 +4,16 @@ import rasterize.Raster;
 
 public class SeedFill implements Filler{
     private Raster raster;
-    private int x, y;
+    private int x, y, xMax, yMax;
     private int backgroundColor;
 
-    public SeedFill(Raster raster, int backgroundColor, int x, int y) {
+    public SeedFill(Raster raster, int backgroundColor, int x, int y, int xMax, int yMax) {
         this.raster = raster;
         this.backgroundColor = backgroundColor;
         this.x = x;
         this.y = y;
+        this.xMax = xMax;
+        this.yMax = yMax;
     }
     @Override
     public void fill() {
@@ -19,16 +21,18 @@ public class SeedFill implements Filler{
     }
 
     private void seedFill(int x, int y) {
-        int pixelColor = raster.getPixel(x, y);
+        if (x > 0 && x < this.xMax && y > 0 && y < this.yMax) {
+            int pixelColor = raster.getPixel(x, y);
 
-        if (this.backgroundColor != pixelColor)
-            return;
+            if (this.backgroundColor != pixelColor)
+                return;
 
-        raster.setPixel(x, y, 0xff00ff);
+            raster.setPixel(x, y, 0xff00ff);
 
-        seedFill(x+1, y);
-        seedFill(x-1, y);
-        seedFill(x, y+1);
-        seedFill(x, y-1);
+            seedFill(x+1, y);
+            seedFill(x-1, y);
+            seedFill(x, y+1);
+            seedFill(x, y-1);
+        }
     }
 }
