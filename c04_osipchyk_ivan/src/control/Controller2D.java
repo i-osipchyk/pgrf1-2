fe366b11ada30dc42e34643e288a55a0398f45c6
Ivan Osipchyk <ivanosipchyk@MacBook-Air.local>
2023-11-17 00:47:23 +0100

    added logic to draw a list of polygons and choose some of them to rasterize

--- c04_osipchyk_ivan/src/control/Controller2D.java
+++ c04_osipchyk_ivan/src/control/Controller2D.java
@@ -28,12 +28,17 @@ public class Controller2D implements Controller {
     public PolygonRasterizer clippingPolygonRasterizer;
     public PolygonClipper clipper;
     private boolean wasDragged = false;
+    private ArrayList<Polygon> polygons = new ArrayList<>();
+    private Polygon currentPolygon;
+    private final ColorGenerator colorGenerator = new ColorGenerator();
+    private int polygonToRasterize = -1;
+    private StringBuilder numberInput = new StringBuilder();
     Point startingPoint;
     Point movingPoint;
     private int rasterizationMode = 1;
     private int fillingMode = 1;
     private int clippingMode = 0;
-    private Color finalLineColor = Color.GREEN;
+    public Color finalLineColor = Color.GREEN;
     private Color movingLineColor = Color.RED;
     private Color clippingFinalLineColor = Color.BLUE;
     private Color clippingMovingLineColor = Color.CYAN;
@@ -99,10 +104,40 @@ public class Controller2D implements Controller {
                             movingPoint = new Point(e.getX(), e.getY());
                         }
                     }
+
+                    if (rasterizationMode == 3) {
+                        if (e.getButton() == MouseEvent.BUTTON1) {
+                            if (currentPolygon.getPoints().isEmpty()) {
+                                currentPolygon.addPoint(new Point(e.getX(), e.getY()));
+                                movingPoint = new Point(e.getX(), e.getY());
+                            }
+                        }
+
+                        if (e.getButton() == MouseEvent.BUTTON3) {
+                            if (fillingMode == 1) {
+                                if (polygonToRasterize > 0 && polygonToRasterize <= polygons.size()) {
+                                    ScanLine scanLine = new ScanLine(lineRasterizer, polygonRasterizer, polygons.get(polygonToRasterize-1));
+                                    scanLine.fill();
+                                } else {
+                                    System.out.println("There is no such polygon drawn");
+                                }
+                                polygonToRasterize = -1;
+                                numberInput.setLength(0);
+                            }
+                        }
+                    }
                 } else {
                     if (e.getButton() == MouseEvent.BUTTON1) {
                         if (clippingPolygon.getPoints().isEmpty()) {
                             clippingPolygon.addPoint(new Point(e.getX(), e.getY()));
+                        }
+                    }
+
+                    if (e.getButton() == MouseEvent.BUTTON3) {
+                        if (fillingMode == 1) {
+                            ScanLine scanLine = new ScanLine(lineRasterizer, polygonRasterizer, polygon);
+                            scanLine.fill();
+                            polygonToRasterize = -1;
                         }
                     }
                 }
@@ -133,6 +168,16 @@ public class Controller2D implements Controller {
                         if (wasDragged)
                             update();
                         movingPoint = null;
+                    }
+
+                    if (rasterizationMode == 3) {
+                        if (e.getButton() == MouseEvent.BUTTON1) {
+                            if (wasDragged || currentPolygon.getPoints().size() != 1) {
+                                currentPolygon.addPoint(new Point(x, y));
+                            }
+                            movingPoint = null;
+                            update();
+                        }
                     }
                 } else {
                     if (e.getButton() == MouseEvent.BUTTON1) {
@@ -168,6 +213,13 @@ public class Controller2D implements Controller {
                         movingPoint = new Point(x, y);
                         update();
                     }
+
+                    if (rasterizationMode == 3) {
+                        if (e.getButton() == MouseEvent.BUTTON1) {
+                            movingPoint = new Point(x, y);
+                            update();
+                        }
+                    }
                 } else {
                     if (e.getButton() == MouseEvent.BUTTON1) {
                         movingPoint = new Point(x, y);
@@ -197,17 +249,17 @@ public class Controller2D implements Controller {
                     clippingMode = 0;
                 }
 
-                if(e.getKeyChar() == '1') {
+                if(e.getKeyChar() == 'b' || e.getKeyChar() == 'B') {
                     fillingMode = 1;
                     clippingMode = 0;
                 }
 
-                if(e.getKeyChar() == '2') {
+                if(e.getKeyChar() == 'n' || e.getKeyChar() == 'N') {
                     fillingMode = 2;
                     clippingMode = 0;
                 }
 
-                if(e.getKeyChar() == '3') {
+                if(e.getKeyChar() == 'm' || e.getKeyChar() == 'M') {
                     fillingMode = 3;
                     clippingMode = 0;
                 }
@@ -223,6 +275,34 @@ public class Controller2D implements Controller {
                 if(e.getKeyChar() == 'e' || e.getKeyChar() == 'E') {
                     clipPolygon(polygon, clippingPolygon);
                 }
+
+                if(e.getKeyChar() == 'l' || e.getKeyChar() == 'L') {
+                    rasterizationMode = 3;
+                    clippingMode = 0;
+                }
+
+                if(e.getKeyChar() == 'a' || e.getKeyChar() == 'A') {
+                    Color color = colorGenerator.generate();
+                    currentPolygon = new Polygon(color);
+                    polygons.add(currentPolygon);
+                }
+
+                if (Character.isDigit(e.getKeyChar())) {
+                    polygonToRasterize = Character.getNumericValue(e.getKeyChar());
+                }
+                if (Character.isDigit(e.getKeyChar())) {
+                    numberInput.append(e.getKeyChar());
+                    try {
+                        int enteredNumber = Integer.parseInt(numberInput.toString());
+                        polygonToRasterize = enteredNumber;
+                    } catch (NumberFormatException ex) {
+                        System.out.println("Invalid input: " + numberInput);
+                    }
+                }
+
+                if (!Character.isDigit(e.getKeyChar())) {
+                    numberInput.setLength(0);
+                }
             }
         });
     }
@@ -231,7 +311,7 @@ public class Controller2D implements Controller {
         panel.clear();
         if (clippingMode == 0) {
             if (rasterizationMode == 1)
-                polygonRasterizer.rasterize(polygon, movingPoint);
+                polygonRasterizer.rasterize(polygon, movingPoint, finalLineColor);
             if (rasterizationMode == 2) {
                 rectangleRasterizer.rasterize(rectangle, startingPoint, movingPoint);
                 if (rectangle.getPoints().size() == 4) {
@@ -239,10 +319,18 @@ public class Controller2D implements Controller {
                     ellipseRasterizer.rasterize();
                 }
             }
+            if (rasterizationMode == 3 && !polygons.isEmpty()) {
+                for (Polygon polygon: polygons) {
+                    if (polygons.indexOf(polygon) == polygons.size() - 1)
+                        polygonRasterizer.rasterize(polygon, movingPoint, polygon.color);
+                    else
+                        polygonRasterizer.rasterize(polygon, null, polygon.color);
+                }
+            }
         } else {
             if (rasterizationMode == 1)
-                polygonRasterizer.rasterize(polygon, null);
-            clippingPolygonRasterizer.rasterize(clippingPolygon, movingPoint);
+                polygonRasterizer.rasterize(polygon, null, finalLineColor);
+            clippingPolygonRasterizer.rasterize(clippingPolygon, movingPoint, clippingFinalLineColor);
         }
     }
 
@@ -251,15 +339,20 @@ public class Controller2D implements Controller {
     }
 
     private void cleanAll() {
-        polygon = new Polygon();
-        polygonRasterizer = new PolygonRasterizer(lineRasterizer, finalLineColor, movingLineColor);
+        polygon = new Polygon(finalLineColor);
+        polygonRasterizer = new PolygonRasterizer(lineRasterizer, movingLineColor);
 
-        rectangle = new Rectangle();
+        rectangle = new Rectangle(Color.RED);
         rectangleRasterizer = new RectangleRasterizer(lineRasterizer);
         ellipseRasterizer = new EllipseRasterizer(lineRasterizer);
 
-        clippingPolygon = new Polygon();
-        clippingPolygonRasterizer = new PolygonRasterizer(lineRasterizer, clippingFinalLineColor, clippingMovingLineColor);
+        clippingPolygon = new Polygon(clippingFinalLineColor);
+        clippingPolygonRasterizer = new PolygonRasterizer(lineRasterizer, clippingMovingLineColor);
+
+        Color color = colorGenerator.generate();
+        currentPolygon = new Polygon(color);
+        polygons = new ArrayList<>();
+//        polygons.add(currentPolygon);
 
         movingPoint = null;
         panel.clear();
@@ -274,10 +367,4 @@ public class Controller2D implements Controller {
         update();
     }
 
-    private void cleanRectangle() {
-        rectangle = new Rectangle();
-        rectangleRasterizer = new RectangleRasterizer(lineRasterizer);
-        ellipseRasterizer = new EllipseRasterizer(lineRasterizer);
-        movingPoint = null;
-    }
 }
