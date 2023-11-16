487ef86b3c51086af8526420150a957f20e7a616
Ivan Osipchyk <ivanosipchyk@MacBook-Air.local>
2023-11-17 00:45:25 +0100

    added color to the construtor

--- c04_osipchyk_ivan/src/model/Rectangle.java
+++ c04_osipchyk_ivan/src/model/Rectangle.java
@@ -1,22 +1,23 @@
 package model;
 
+import java.awt.*;
 import java.util.ArrayList;
 
 public class Rectangle extends Polygon {
     private ArrayList<Point> points;
 
-    public Rectangle(Point p1, Point p3) {
-        Point p2 = new Point(p1.x, p3.y);
-        Point p4 = new Point(p3.x, p1.y);
+//    public Rectangle(Point p1, Point p3) {
+//        Point p2 = new Point(p1.x, p3.y);
+//        Point p4 = new Point(p3.x, p1.y);
+//
+//        addPoint(p1);
+//        addPoint(p2);
+//        addPoint(p3);
+//        addPoint(p4);
+//    }
 
-        addPoint(p1);
-        addPoint(p2);
-        addPoint(p3);
-        addPoint(p4);
-    }
-
-    public Rectangle() {
-
+    public Rectangle(Color color) {
+        super(color);
     }
 
     public void addAllPoints(Point p1, Point p3) {
