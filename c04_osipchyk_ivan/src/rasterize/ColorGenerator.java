9eb6f1453a066a520b4e52296cfb923216cb8a20
Ivan Osipchyk <ivanosipchyk@MacBook-Air.local>
2023-11-17 00:45:54 +0100

    added random color generator color

--- c04_osipchyk_ivan/src/rasterize/ColorGenerator.java
+++ c04_osipchyk_ivan/src/rasterize/ColorGenerator.java
@@ -1,2 +1,34 @@
-package rasterize;public class ColorGenerator {
+package rasterize;
+import java.awt.Color;
+import java.util.Random;
+
+public class ColorGenerator {
+
+    public ColorGenerator() {
+
+    }
+
+    public Color generate() {
+        Random random = new Random();
+        int maxAttempts = 1000;
+
+        for (int attempt = 0; attempt < maxAttempts; attempt++) {
+            int red = random.nextInt(256);
+            int green = random.nextInt(256);
+            int blue = random.nextInt(256);
+
+            Color randomColor = new Color(red, green, blue);
+
+            if (!isTooDark(randomColor, 100)) {
+                return randomColor;
+            }
+        }
+
+        return Color.WHITE;
+    }
+
+    private boolean isTooDark(Color color, int darknessThreshold) {
+        double brightness = (0.2126 * color.getRed()) + (0.7152 * color.getGreen()) + (0.0722 * color.getBlue());
+        return brightness < darknessThreshold;
+    }
 }
