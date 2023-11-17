7ac1fc53286c82f314b8a64ce86cdbfd1db95c1d
Ivan Osipchyk <ivanosipchyk@MacBook-Air.local>
2023-11-17 14:35:31 +0100

    added functions for moving nearest point

--- c04_osipchyk_ivan/src/model/Polygon.java
+++ c04_osipchyk_ivan/src/model/Polygon.java
@@ -53,4 +53,32 @@ public class Polygon {
             }
         }
     }
+    public void setPoint(int index, Point point) {
+        if(index >= 0 && index < points.size())
+            points.set(index, point);
+    }
+    public int findNearestPointIndex(Point point, double tolerance) {
+        int nearest = -1;
+        for(int i = 0; i < points.size(); i++) {
+            Point current = points.get(i);
+
+            Point relative = new Point(point.x - current.x, point.y - current.y);
+            double length  = Math.sqrt((float)Math.pow(relative.x, 2) + Math.pow(relative.y, 2));
+
+            if(length < tolerance) {
+
+                if(nearest < 0)
+                    nearest = i;
+
+                Point nearestPoint = points.get(nearest);
+                Point tmp = new Point(point.x - nearestPoint.x, point.y - nearestPoint.y);
+                double nearestLength = Math.sqrt((float)Math.pow(tmp.x, 2) + Math.pow(tmp.y, 2));
+
+                if(length < nearestLength)
+                    nearest = i;
+            }
+        }
+
+        return nearest;
+    }
 }
